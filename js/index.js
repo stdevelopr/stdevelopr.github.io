ef8d9b0613cb8ac8163b355cae356c03a67c09b3
stdevelopr <stdevelopr@gmail.com>
2018-05-01 22:02:46 -0300

    new layout

--- js/index.js
+++ js/index.js
@@ -19,8 +19,6 @@ particlesJS("first_section",
 	"remove":{"particles_nb":2}}},
 	"retina_detect":true});
 var count_particles, stats, update; 
-stats = new Stats; 
-stats.setMode(0); 
 stats.domElement.style.position = 'absolute'; 
 stats.domElement.style.left = '0px'; 
 stats.domElement.style.top = '0px'; 
