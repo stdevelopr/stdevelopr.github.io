08dcfefd64b653bd3b30db6eb4e20eafc16e2b12
stdevelopr <stdevelopr@gmail.com>
2018-05-01 22:16:59 -0300

    ko

--- js/index.js
+++ js/index.js
@@ -19,16 +19,9 @@ particlesJS("first_section",
 	"remove":{"particles_nb":2}}},
 	"retina_detect":true});
 var count_particles, stats, update; 
-stats.domElement.style.position = 'absolute'; 
-stats.domElement.style.left = '0px'; 
-stats.domElement.style.top = '0px'; 
-document.body.appendChild(stats.domElement); 
 update = function() { 
-	stats.begin(); 
-	stats.end(); 
-	if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { 
-		count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; 
-	} 
+
+	
 	requestAnimationFrame(update); 
 }; 
 requestAnimationFrame(update);
